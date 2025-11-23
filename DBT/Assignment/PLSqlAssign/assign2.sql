af4e4e26b2b6f4aa484a29cab276de73a6db5ce1
shashank-77-77 <shashankmankar412@gmail.com>
2025-11-23 22:51:41 +0530

    DBT PLSQL ASSNG CMPLT

--- DBT/Assignment/PLSqlAssign/assign2.sql
+++ DBT/Assignment/PLSqlAssign/assign2.sql
@@ -1,15 +1,15 @@
-drop procedure if EXISTS place_order;
- DELIMITER $
- CREATE PROCEDURE place_order (IN p_customer_id INT, IN p_product_id INT, IN p_qty INT)
  /*•	Create a procedure place_order(p_customer_id, p_product_id, p_qty)
 •	Check if stock is available.
 •	If yes, reduce stock, insert into orders, and calculate bill with 18% GST.
 •	If not, raise an exception.
 •	Assign random discounts between 5% and 30% whenever a product is added.
 •	Whenever a new order is placed, assign a random cashback (0 to ₹100).
-
 */
 
+drop procedure if EXISTS place_order;
+ DELIMITER $
+ CREATE PROCEDURE place_order (IN p_customer_id INT, IN p_product_id INT, IN p_qty INT)
+ 
  BEGIN
  -- •	Check if stock is available.
  DECLARE stock_bal int;
@@ -42,21 +42,3 @@ END IF;
  END $
  DELIMITER ;
  
- /*+------------+--------------+-------+-------+
-| product_id | product_name | price | stock |
-+------------+--------------+-------+-------+
-|        100 | bread        |    30 |    10 |
-|        101 | jam          |    50 |    20 |
-|        102 | butter       |    60 |    15 |
-|        103 | biscuit      |    10 |    25 |
-|        104 | chocolate    |   100 |     5 |
-+------------+--------------+-------+-------+
-5 rows in set (0.00 sec)*/
- 
- /*+----------+-------------+------------+------+-------------+------------+
-|  | customer_id | product_id | qty  | total_price | order_date |
-+----------+-------------+------------+------+-------------+------------+
-|        1 |         200 |        100 |    2 |          60 | 2025-11-13 |
-|        2 |         201 |        101 |    3 |         120 | 2025-11-13 |
-+----------+-------------+------------+------+-------------+------------+
-2 rows in set (0.00 sec)*/
